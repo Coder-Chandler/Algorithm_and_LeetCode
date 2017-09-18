bb68faa72a6e5a2d68a070c18c16db2f1433f629
Coder-Chandler <ysiwgtus@gmail.com>
2017-09-18 23:14:08 +0800

    优化
    优化

--- Data_size_concept/Power.py
+++ Data_size_concept/Power.py
@@ -5,11 +5,9 @@
 import time
 def Power():
     for i in range(1, 10):
-        n = 10**i # 计算10的i次幂
+        n = pow(10, i) # 计算10的i次幂
         start_time = time.time()
-        count = 0
-        for x in range(0, n):
-            count += x
+        sum(x for x in range(1, n+1))
         end_time = time.time()
         print("10^%d:%fs"%(i, (end_time-start_time)))
 Power()
